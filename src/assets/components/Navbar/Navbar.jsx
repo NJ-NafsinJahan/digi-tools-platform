642430c15553b4a12af067e12bcd52f29812ebdd
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-03-31 07:40:45 +0600

    add component and picture

---
+++ src/assets/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <h2 className="text-2xl text-red-600">Hello react </h2>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-secondary">Navbar</button>
+    </>
+  );
+};
+
+export default Navbar;
