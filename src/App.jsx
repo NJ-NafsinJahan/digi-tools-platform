432536b4b440eaa40e3124bc7f99a6eda004325a
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-03-31 11:46:45 +0600

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 
 import "./App.css";
 import Navbar from "./assets/components/Navbar/Navbar";
+import Banner from "./assets/components/Banner/Banner";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  );
 }
 
 export default App;
--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,11 +1,79 @@
 import React from "react";
+import logo from "../../picture/DigiTools.png";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Navbar = () => {
   return (
     <>
-      <h2 className="text-2xl text-red-600">Hello react </h2>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-secondary">Navbar</button>
+      <div className="navbar bg-base-100 shadow-sm px-30 py-6">
+        <div className="navbar-start ">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            {/* ********** */}
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow mb-4"
+            >
+              <button className="  text-[#101727]"> Products </button>
+              <button className="  text-[#101727]"> Features </button>
+              <button className=" text-[#101727]"> Pricing </button>
+              <button className=" text-[#101727]"> Testimonials </button>
+              <button className="  text-[#101727]"> FAQ </button>
+            </ul>
+            {/* ************** */}
+          </div>
+          <img src={logo} alt="" />
+        </div>
+        <div className="navbar-center hidden lg:flex gap-8 px-8">
+          <button className=" navbar-center  font-semibold text-[18px] text-[#101727]">
+            {" "}
+            Products{" "}
+          </button>
+          <button className=" font-semibold text-[18px] text-[#101727]">
+            {" "}
+            Features{" "}
+          </button>
+          <button className=" font-semibold text-[18px] text-[#101727]">
+            {" "}
+            Pricing{" "}
+          </button>
+          <button className=" font-semibold text-[18px] text-[#101727]">
+            {" "}
+            Testimonials{" "}
+          </button>
+          <button className=" font-semibold text-[18px] text-[#101727]">
+            {" "}
+            FAQ{" "}
+          </button>
+        </div>
+        <div className="navbar-end gap-4">
+          <div className=" p-2">
+            <FiShoppingCart />
+          </div>
+          <button className=" font-semibold text-[18px] text-[#101727]">
+            Login
+          </button>
+          <button className="btn  font-semibold text-[18px] text-white rounded-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA]  border-none px-4 py-6 ">
+            Get Started
+          </button>
+        </div>
+      </div>
     </>
   );
 };
